89db1f238f974c95ec6925ef40cdece8ff7b80d7
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-11 06:50:39 +0530

    Changes

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-aws ssm describe-parameters
+for parameter in "catalogue.prod.MONGO_URL,MONGO_URL catalogue.prod.DOCUMENTDB,DOCUMENTDB"; do
+  ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
+  app_parameter=$(echo $parameter | awk -F , '{print $2}')
+  aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g'
+done
