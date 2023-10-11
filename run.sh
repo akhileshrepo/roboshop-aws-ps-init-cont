cd522d855ae8371844ae943b89957e585ceb0b6e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-11 07:16:45 +0530

    Changes

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-for parameter in "catalogue.prod.MONGO_URL,MONGO_URL catalogue.prod.DOCUMENTDB,DOCUMENTDB"; do
+for parameter in $PARAMETERS; do
   ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
   app_parameter=$(echo $parameter | awk -F , '{print $2}')
-  aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g'
+  echo export ${app_parameter}=\"$(aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/common/parameters
 done
+
+cat /common/parameters
